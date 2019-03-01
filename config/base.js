19a82b0e00ede2f61bf25d0c2557f97df3946ed7
Ilya Lytvynov <ilyaillych@gmail.com>
2019-03-01 22:06:51 +0200

    Lesson 11

--- config/base.js
+++ config/base.js
@@ -9,7 +9,8 @@ const baseConf = (_path) => {
     // and add path and name to entry array
     const VENDORS_NAME = 'vendors';
     const entry = {
-        index: ['babel-polyfill', './src/index/index.js']
+        index: ['babel-polyfill', './src/index/index.js'],
+        'lesson_11': ['./src/lesson_11/lesson_11.js'],
     };
 
     const plugins = Object.keys(entry).reduce((acc, name) => {
